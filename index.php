c48eb0620e7a8ac2576cfebbcd35aba246c59c72
Yago Alves Gomes <yagogomesc@gmail.com>
2016-04-01 14:36:10 -0300

    Adicionado numero de comentarios de cada noticia

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 	$titulo_pagina = "Inicio";
 	include ('header.php');
 	
+	if(isset($_SESSION['usuario_id'])){
 	require_once('../mysqli_phpconnect.php');
 	
 	echo '<h1 align="center">Ultimas notícias</h1>';
@@ -13,14 +14,23 @@
 		echo '<br />';
 		
 		while($row = mysqli_fetch_array($r, MYSQLI_ASSOC)){
+				$noid = $row['noticia_id'];
+				$g = "SELECT COUNT(comentario_id) FROM comentarios WHERE noticia_id='$noid'";
+				$c = mysqli_query($dbc, $g);
+				
+				$coment_num = mysqli_fetch_array($c, MYSQLI_NUM);
+				
 				$resumoCorpo = substr($row['corpo'], 0, strrpos(substr($row['corpo'], 0, 480), ' ')) . '...';
 				
 				echo '<h2><a href="ver_noticia.php?nid='.$row['noticia_id'].'">'.$row['titulo'].'</a></h2>';
-				echo '<p align="justify">'.$resumoCorpo.'</p>';
-				echo '<div class="lermais" align="right"><table style="border: 1px double"><td><a href="ver_noticia.php?nid='.$row['noticia_id'].'">Ler mais.</a></td></table></div>';
+				echo '<p align="justify" style="margin-top: -10px">'.$resumoCorpo.'</p>';
+				echo '  <div class="lermais" ><span style="color: grey; font-style: italic">'.$coment_num[0].' comentário(s)</span><a href="ver_noticia.php?nid='.$row['noticia_id'].'"><div style="border: 1px double; float: right; padding: 2px; margin-right: 4px">Ler mais.</div></a></div>';
 		}	
 	}else{
 		echo '<p>Não foi encontrado nenhuma notícia. :( </p>';
+	}
+	}else{
+		header("Location: login.php");
 	}
 ?>
 
