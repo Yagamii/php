589f70180a0bc6bcbdb99ec71e9bc713f4efcdef
Yago Alves Gomes <yagogomesc@gmail.com>
2016-04-21 22:05:52 -0300

    Adicionado funções uteis a classe padrao

--- app/src/default.php
+++ app/src/default.php
@@ -8,6 +8,21 @@
 			$this->Dbc = $Connect->connectToDb();
 		}
 		
+		public function getNumComents($nid){
+				$noid = $nid;
+				$g = "SELECT COUNT(comentario_id) FROM comentarios WHERE noticia_id='$noid'";
+				$c = $this->Dbc->query($g);
+				
+				$coment_num = mysqli_fetch_array($c, MYSQLI_NUM);
+				
+				return $coment_num;
+		}
+		
+		public function resumoCorpo($corpo){
+			$resumoCorpo = substr($corpo, 0, strrpos(substr($corpo, 0, 480), ' ')) . '...';
+			
+			return $resumoCorpo;
+		}
 	}
 	
 ?>
